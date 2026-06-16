31cd5fe19d8d6c8c2885d07dd7b4a75f6c58dc02
DeepyCat <105164030+DeepyCat@users.noreply.github.com>
2026-06-16 17:45:28 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -65,7 +65,7 @@ async function checkGuess() {
 
     document.getElementById('gameBoard').prepend(attemptRow);
     input.value = '';
-    
+   
 }
 
 document.getElementById('guessBtn').addEventListener('click', checkGuess);
